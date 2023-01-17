7bd131170cad3e97f0b4076472ca71cf6d20afb8
Hosseinreza <hrakbari77@gmail.com>
2023-01-17 16:19:06 +0330

    Add loop for drum buttons

---
+++ index.js
@@ -0,0 +1,9 @@
+const numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for ( var i = 0 ; i < numberOfDrumButtons ; i++ ){
+
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("clicked");
+    });
+
+}
