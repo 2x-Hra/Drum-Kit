07dc33f1a2295bbbeb8a3b8a4118d8c9e45e3864
Hosseinreza <hrakbari77@gmail.com>
2023-01-18 14:37:29 +0330

    Add sounds

--- index.js
+++ index.js
@@ -3,7 +3,42 @@ for ( var i = 0 ; i < numberOfDrumButtons ; i++ ){
 
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("clicked");
+        
+        var ButtonInnerHtml = this.textContent;
+        
+        switch (ButtonInnerHtml) {
+            case "w":
+                var tom1 = new Audio ("sounds/tom-1.mp3")
+                tom1.play();
+                break;
+            case "a":
+                var tom1 = new Audio ("sounds/tom-2.mp3")
+                tom1.play();
+                break;
+                
+            case "s":
+                var tom1 = new Audio ("sounds/tom-3.mp3")
+                tom1.play();
+                break;       
+            case "d":
+                var tom1 = new Audio ("sounds/tom-4.mp3")
+                tom1.play();
+                break;
+            case "j":
+                var tom1 = new Audio ("sounds/snare.mp3")
+                tom1.play();
+                break;
+            case "k":
+                var tom1 = new Audio ("sounds/crash.mp3")
+                tom1.play();
+                break;
+            case "l":
+                var tom1 = new Audio ("sounds/kick-bass.mp3")
+                tom1.play();
+                break;
+            default:
+                break;
+        }
     });
 
 }
